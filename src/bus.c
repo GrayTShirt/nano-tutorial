4d051ee30d0051c38aa4921ebeb35847c245e76a
Dan <dan@d3fy.net>
2014-10-23 08:35:50 -0400

    Bus can use epoll now

--- src/bus.c
+++ src/bus.c
@@ -7,15 +7,13 @@
 #include <nanomsg/nn.h>
 #include <nanomsg/bus.h>
 
-#define MAXEVENTS 1024
+#define MAX_EVENTS 16
 
 int node (const int argc, const char **argv)
 {
 	int sock    = nn_socket (AF_SP, NN_BUS);
-	int sock_fd;
-	int efd;
-	struct epoll_event event;
-	struct epoll_event *events;
+	int sock_fd, efd, nfds;
+	struct epoll_event event, events[MAX_EVENTS];
 	size_t sz = sizeof(sock_fd);
 	nn_getsockopt(sock, NN_SOL_SOCKET, NN_RCVFD, &sock_fd, &sz);
 	assert(sock >= 0);
@@ -23,39 +21,54 @@ int node (const int argc, const char **argv)
 	sleep (1); // wait for connections
 	if (argc >= 3) {
 		int x;
-		for(x = 3; x<argc; x++)
-			assert (nn_connect (sock, argv[x]) >= 0);
+		for(x = 3; x < argc; x++)
+			assert(nn_connect(sock, argv[x]) >= 0);
 	}
-	efd = epoll_create1 (0);
-	/* if (efd == -1) {
-	 *	perror ("epoll_create");
-	 *	abort ();
-	 * }
-	 */
+	efd = epoll_create(MAX_EVENTS);
 	assert(efd != -1);
 
 	event.data.fd = sock_fd;
-	event.events = EPOLLIN | EPOLLET;
-	epoll_ctl(efd, EPOLL_CTL_ADD, sock_fd, &event);
-	// assert(s != -1);
+	// event.events = EPOLLIN | EPOLLET;
+	event.events = EPOLLIN; // nanomsg is level triggered.
+	assert(epoll_ctl(efd, EPOLL_CTL_ADD, sock_fd, &event) != -1);
+	// events = malloc(MAX_EVENTS * sizeof(event));
 
-	/* Buffer where events are returned */
-	events = calloc (MAXEVENTS, sizeof event);
-	sleep (1); // wait for connections
+	sleep(1); // wait for connections
 	int to = 100;
-	assert (nn_setsockopt (sock, NN_SOL_SOCKET, NN_RCVTIMEO, &to, sizeof (to)) >= 0);
+	assert(nn_setsockopt (sock, NN_SOL_SOCKET, NN_RCVTIMEO, &to, sizeof(to)) >= 0);
+
 	// SEND
 	int sz_n = strlen(argv[1]) + 1; // '\0' too
 	printf ("%s: SENDING '%s' ONTO BUS\n", argv[1], argv[1]);
 	int send = nn_send (sock, argv[1], sz_n, 0);
 	assert (send == sz_n);
-	while (1) {
-		// RECV
-		char *buf = NULL;
-		int recv = nn_recv (sock, &buf, NN_MSG, 0);
-		if (recv >= 0) {
-			printf ("%s: RECEIVED '%s' FROM BUS\n", argv[1], buf);
-			nn_freemsg (buf);
+
+	for(;;) {
+		nfds = epoll_wait(efd, events, MAX_EVENTS, -1);
+		assert(nfds != -1);
+
+		int n, recv;
+		for (n = 0; n < nfds; ++n) {
+			if (events[n].data.fd == sock_fd) {
+				char *buf = NULL;
+				recv = nn_recv(sock, &buf, NN_MSG, 0);
+				if (recv >= 0) {
+					printf ("%s: RECEIVED '%s' FROM BUS\n", argv[1], buf);
+					nn_freemsg (buf);
+				}
+				/*
+				event.events = EPOLLIN | EPOLLET;
+				event.data.fd = conn_sock;
+				if (epoll_ctl(efd, EPOLL_CTL_ADD, conn_sock,
+					&event) == -1) {
+
+					perror("epoll_ctl: conn_sock");
+					exit(EXIT_FAILURE);
+				} else {
+					do_use_fd(events[n].data.fd);
+				}
+				*/
+			}
 		}
 	}
 	return nn_shutdown (sock, 0);

e095b7b196c5743cf006350f68a80a126ea8002c
Dan <dan@d3fy.net>
2014-10-23 06:48:50 -0400

    Slightly better buffer size management

--- src/time.c
+++ src/time.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <time.h>
 #include <string.h>
+#include <stdlib.h>
+
+#define STR_BUF 30
 
 char* date ()
 {
 	struct tm *tmp = malloc(sizeof(struct tm));
-	char *ttime = malloc(30);
+	char *ttime = malloc(STR_BUF);
 	time_t t = time(NULL);
 	tmp = localtime(&t);
-	strftime( ttime, 30, "%Y%m%d %X", tmp);
-	printf("Time: %s\n", ttime);
+	strftime( ttime, STR_BUF, "%Y%m%d %X", tmp);
 	return ttime;
 }
 
